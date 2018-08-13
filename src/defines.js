656fa755eba7aa15fa7924abdcf2f6def62181bb
boushi-bird <boushi-bird@users.noreply.github.com>
2018-08-14 01:57:56 +0900

    データ取得先URL変更

--- src/defines.js
+++ src/defines.js
@@ -6,7 +6,7 @@ const hubPageUrl = process.env.NODE_ENV === 'production'
   : 'https://localhost:8443/hub/'
 const scriptId = 'FjwVBjy6'
 const baseDataUrl = process.env.NODE_ENV === 'production'
-  ? 'https://gist.githubusercontent.com/boushi-bird/9fbed1f50fadcd04ea619355b5fa7a0c/raw/facc4e3b29a79ed477dd7e2a477eab656df0c0a4/base.json'
+  ? 'https://gist.githubusercontent.com/boushi-bird/9fbed1f50fadcd04ea619355b5fa7a0c/raw/base.json'
   : '/base.json'
 
 export {
