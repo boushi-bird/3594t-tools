dc586c159cae8c36f058f205e1be4a47f7fe6a56
boushi-bird <boushi-bird@users.noreply.github.com>
2019-03-02 18:40:02 +0900

    tab部分切り出し

--- src/deck/components/CardFilter/CardFilter.tsx
+++ src/deck/components/CardFilter/CardFilter.tsx
@@ -1,73 +1,48 @@
 import './CardFilter.css';
 import React from 'react';
 import classNames from 'classnames';
+import FilterTabs from './FilterTabs';
 
 const tabs = {
   BASIC: '基本',
   DETAIL: '詳細',
 };
 
-type FilterTab = keyof typeof tabs;
+export type TabType = keyof typeof tabs;
 
 interface Props {
   open: boolean;
   onClose: () => void;
 }
 
-interface State {
-  activeTab: FilterTab;
+interface LocalState {
+  activeTab: TabType;
 }
 
-export default class CardFilter extends React.Component<Props, State> {
-  public state: State = {
+export default class CardFilter extends React.PureComponent<Props, LocalState> {
+  public state: LocalState = {
     activeTab: 'BASIC',
   };
 
-  private handleTabClick = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ): void => {
-    const activeTab = event.currentTarget.dataset['tab'] as FilterTab;
-    if (this.state.activeTab !== activeTab) {
-      this.setState({ activeTab });
-    }
+  private handleTabChange = (activeTab: TabType): void => {
+    this.setState({ activeTab });
   };
-
-  private handleOkClick = (): void => {
-    this.props.onClose();
-  };
-
-  private renderTabs(): React.FC {
-    const buttons: JSX.Element[] = [];
-    Object.entries(tabs).forEach(([key, value]) => {
-      const active = this.state.activeTab === key;
-      const buttonClass = `tab-button-${key.toLowerCase()}`;
-      const buttonClasses = classNames([buttonClass, { active }]);
-      buttons.push(
-        <button
-          key={key}
-          data-tab={key}
-          className={buttonClasses}
-          onClick={this.handleTabClick}
-        >
-          {value}
-        </button>
-      );
-    });
-    return () => <div className="card-filter-tabs">{buttons}</div>;
-  }
 
   public render(): React.ReactNode {
-    const { open } = this.props;
+    const { open, onClose } = this.props;
     const containerClasses = classNames(['card-filter-container', { open }]);
-    const Tabs = this.renderTabs();
     return (
       <div className={containerClasses}>
         <h1 className="card-filter-title">絞り込みメニュー</h1>
         <div className="card-filter-buttons">
-          <Tabs />
+          <FilterTabs
+            tabs={tabs}
+            activeTab={this.state.activeTab}
+            onTabChanged={this.handleTabChange}
+          />
           <div className="card-filter-actions">
             <button className="action-buton-reset">リセット</button>
-            <button className="action-buton-ok" onClick={this.handleOkClick}>
+            <button className="action-buton-ok" onClick={onClose}>
               OK
             </button>
           </div>
---
+++ src/deck/components/CardFilter/FilterTabs.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface Props {
+  tabs: { [key: string]: string };
+  activeTab: string;
+  onTabChanged: (tab: string) => void;
+}
+
+export default class FilterTabs extends React.PureComponent<Props> {
+  private handleTabClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    const activeTab = event.currentTarget.dataset['tab'];
+    if (activeTab) {
+      this.props.onTabChanged(activeTab);
+    }
+  };
+
+  public render(): React.ReactNode {
+    const { tabs, activeTab } = this.props;
+    const buttons: JSX.Element[] = [];
+    Object.entries(tabs).forEach(([key, value]) => {
+      const active = activeTab === key;
+      const buttonClass = `tab-button-${key.toLowerCase()}`;
+      const buttonClasses = classNames([buttonClass, { active }]);
+      buttons.push(
+        <button
+          key={key}
+          data-tab={key}
+          className={buttonClasses}
+          onClick={this.handleTabClick}
+        >
+          {value}
+        </button>
+      );
+    });
+    return <div className="card-filter-tabs">{buttons}</div>;
+  }
+}
